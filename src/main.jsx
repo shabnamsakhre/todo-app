00e90afb8fce6a8f51a7055e8c330fdfdd26eda8
shabnamsakhre <sakhreshabnam@gmail.com>
2025-05-22 15:18:03 +0530

    Context API - 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import Wrapper from "./Wrapper.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <ToastContainer />
-  </>
-);
+createRoot(document.getElementById("root")).render(<Wrapper App={<App />} />);
+
+// <>
+//   <App />
+//   <ToastContainer />
+// </>

6900a27ab271fdd942b3322ee0fcba8248608b3f
shabnamsakhre <sakhreshabnam@gmail.com>
2025-05-22 21:25:01 +0530

    Complete Project - Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import "./index.css";
 import { todoContext } from "./Wrapper";
 
 const App = () => {
-  const [todos, setTodos] = useContext(todoContext);
-
   return (
     <div className="w-screen h-screen bg-gray-800 text-white p-10 flex">
-      <Create todos={todos} setTodos={setTodos} />
-      <Read todos={todos} setTodos={setTodos} />
+      <Create />
+      <Read />
     </div>
   );
 };
